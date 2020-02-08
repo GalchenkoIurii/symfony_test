008b86b3148f6b7958a5c111c1903e5c09e0dbdf
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-02-08 20:18:46 +0200

    form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Notes;
-
+use App\Form\NotesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,15 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $note = new Notes();
+        $form = $this->createForm(NotesType::class, $note);
+        $form->handleRequest($request);
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'form'            => $form->createView()
         ]);
     }
 
--- src/Form/NotesType.php
+++ src/Form/NotesType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Notes;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +17,16 @@ class NotesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('status')
-            ->add('created')
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'Active'   => true,
+                    'Inactive' => false
+                ]
+            ])
+            ->add('created', DateTimeType::class)
+            ->add('submit', SubmitType::class)
         ;
     }
 
